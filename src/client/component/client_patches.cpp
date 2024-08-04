3c7066e4be60dcad2f195ddb9e8cd45ac729d980
ViktorSMI <31064630+ViktorSMI@users.noreply.github.com>
2024-08-04 14:18:21 +0800

    theater load steam usermap

--- src/client/component/client_patches.cpp
+++ src/client/component/client_patches.cpp
@@ -11,6 +11,7 @@
 
 #include <mmeapi.h>
 #include "command.hpp"
+#include "workshop.hpp"
 
 namespace client_patches
 {
@@ -52,6 +53,13 @@ namespace client_patches
 				std::filesystem::path path_to_map_folder = "usermaps";
 				if (std::filesystem::exists(path_to_map_folder / mapname)) {
 					utils::hook::copy_string(0x1567D9A24_g, mapname);
+				}
+				else
+				{
+					auto id = workshop::get_usermap_publisher_id(mapname);
+
+					utils::hook::copy_string(0x1567D9A24_g, id.c_str());
+					utils::hook::copy_string(0x1567D9A04_g, mapname);
 				}
 			}
 			game::Com_GametypeSettings_SetGametype(gametype, true);
